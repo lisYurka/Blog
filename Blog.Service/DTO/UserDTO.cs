7daefd1890687ad1cc2ef6de1db1119586eaa987
Yura <liskovich.99@mail.ru>
2019-06-13 11:14:20 +0300

    WithDTO

---
+++ Blog.Service/DTO/UserDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.Service.DTO
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Mail { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Group { get; set; }
+    }
+}
